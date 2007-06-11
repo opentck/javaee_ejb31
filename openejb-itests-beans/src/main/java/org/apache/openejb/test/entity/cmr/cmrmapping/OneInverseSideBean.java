a5bd5dd0c4e5f52277481cf23eab8cce91d86b16
joes <joes@13f79535-47bb-0310-9956-ffa450edef68>
2007-06-11 22:31:46 +0000

    moving openejb to top-level

---
+++ openejb-itests-beans/src/main/java/org/apache/openejb/test/entity/cmr/cmrmapping/OneInverseSideBean.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.test.entity.cmr.cmrmapping;
+
+import java.util.Set;
+
+import javax.ejb.CreateException;
+
+/**
+ * @version $Revision: 472584 $ $Date: 2006-11-08 10:47:55 -0800 (Wed, 08 Nov 2006) $
+ */
+public abstract class OneInverseSideBean extends AbstractEntityBean {
+
+    // CMP
+    public abstract Integer getId();
+    public abstract void setId(Integer primaryKey);
+
+    // CMR
+    public abstract OneOwningSideLocal getOneOwningSide();
+    public abstract void setOneOwningSide(OneOwningSideLocal oneOwningSideLocal);
+
+    public abstract Set getManyOwningSide();
+    public abstract void setManyOwningSide(Set set);
+
+    public Integer ejbCreate(Integer id) throws CreateException {
+        setId(id);
+        return null;
+    }
+
+    public void ejbPostCreate(Integer id) {
+    }
+}
